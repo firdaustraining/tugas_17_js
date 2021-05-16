14758e0c6d587fe3d92513643e4c2ba3bfad4574
Faza <firdausauliafaza@gmail.com>
2021-05-16 18:02:59 +0700

    niomic.com

---
+++ tugas17.js
@@ -0,0 +1,30 @@
+function tugas1() {
+  var belajar = "Saya beLajar bahaSa peMrograman deNgan khUsuk.";
+  var mancing =
+    "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual";
+  console.log(belajar.toLowerCase());
+  console.log(belajar.toUpperCase());
+  console.log(mancing.toLowerCase());
+  console.log(mancing.toUpperCase());
+}
+tugas1();
+var lorem =
+  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+
+var karakter0 = lorem.charCodeAt(0);
+var karakter1 = lorem.charCodeAt(1);
+var karakter2 = lorem.charCodeAt(2);
+var karakter3 = lorem.charCodeAt(3);
+var karakter4 = lorem.charCodeAt(4);
+var karakter5 = lorem.charCodeAt(5);
+var karakter6 = lorem.charCodeAt(6);
+
+console.log(
+  karakter0,
+  karakter1,
+  karakter2,
+  karakter3,
+  karakter4,
+  karakter5,
+  karakter6
+);
